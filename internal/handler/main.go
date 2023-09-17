8e6220563d89123dd1856a52702ba8264cbb5810
Blaize M Kaye <blaize.kaye@gmail.com>
2023-09-18 09:39:10 +1200

    Removes functional capturing of message obj and turns factory into method

--- internal/handler/main.go
+++ internal/handler/main.go
@@ -259,7 +259,7 @@ func (h *Messaging) Consumer() {
 
 	// Handle any tasks that go to the queue
 	log.Println("Listening for messages in queue lagoon-insights:items")
-	err = messageQueue.SetConsumerHandler("items-queue", processingIncomingMessageQueueFactory(h))
+	err = messageQueue.SetConsumerHandler("items-queue", h.processMessageQueue)
 	if err != nil {
 		log.Println(fmt.Sprintf("Failed to set handler to consumer `%s`: %v", "items-queue", err))
 	}
@@ -285,136 +285,134 @@ func (t *authedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return t.wrapped.RoundTrip(req)
 }
 
-func processingIncomingMessageQueueFactory(h *Messaging) func(mq.Message) {
-	return func(message mq.Message) {
-		var insights InsightsData
-		var resource ResourceDestination
+func (h *Messaging) processMessageQueue(message mq.Message) {
+	var insights InsightsData
+	var resource ResourceDestination
 
-		// set up defer to ack the message after we're done processing
-		defer func(message mq.Message) {
-			// Ack to remove from queue
-			err := message.Ack(false)
-			if err != nil {
-				fmt.Printf("Failed to acknowledge message: %s\n", err.Error())
-			}
-		}(message)
+	// set up defer to ack the message after we're done processing
+	defer func(message mq.Message) {
+		// Ack to remove from queue
+		err := message.Ack(false)
+		if err != nil {
+			fmt.Printf("Failed to acknowledge message: %s\n", err.Error())
+		}
+	}(message)
 
-		incoming := &InsightsMessage{}
-		json.Unmarshal(message.Body(), incoming)
+	incoming := &InsightsMessage{}
+	json.Unmarshal(message.Body(), incoming)
 
-		// if we have direct problems or facts, we process them differently - skipping all
-		// the extra processing below.
-		if incoming.Type == "direct.facts" || incoming.Type == "direct.problems" {
-			resp := processItemsDirectly(message, h)
-			log.Println(resp)
-			return
+	// if we have direct problems or facts, we process them differently - skipping all
+	// the extra processing below.
+	if incoming.Type == "direct.facts" || incoming.Type == "direct.problems" {
+		resp := processItemsDirectly(message, h)
+		log.Println(resp)
+		return
+	}
+
+	// Check labels for insights data from message
+	if incoming.Labels != nil {
+		labelKeys := make([]string, 0, len(incoming.Labels))
+		for k := range incoming.Labels {
+			labelKeys = append(labelKeys, k)
+		}
+		sort.Strings(labelKeys)
+
+		// Set some insight data defaults
+		insights = InsightsData{
+			LagoonType:         Facts,
+			OutputFileExt:      "json",
+			OutputFileMIMEType: "application/json",
 		}
 
-		// Check labels for insights data from message
-		if incoming.Labels != nil {
-			labelKeys := make([]string, 0, len(incoming.Labels))
-			for k := range incoming.Labels {
-				labelKeys = append(labelKeys, k)
+		for _, label := range labelKeys {
+			if label == "lagoon.sh/project" {
+				resource.Project = incoming.Labels["lagoon.sh/project"]
 			}
-			sort.Strings(labelKeys)
-
-			// Set some insight data defaults
-			insights = InsightsData{
-				LagoonType:         Facts,
-				OutputFileExt:      "json",
-				OutputFileMIMEType: "application/json",
+			if label == "lagoon.sh/environment" {
+				resource.Environment = incoming.Labels["lagoon.sh/environment"]
+			}
+			if label == "lagoon.sh/service" {
+				resource.Service = incoming.Labels["lagoon.sh/service"]
 			}
 
-			for _, label := range labelKeys {
-				if label == "lagoon.sh/project" {
-					resource.Project = incoming.Labels["lagoon.sh/project"]
-				}
-				if label == "lagoon.sh/environment" {
-					resource.Environment = incoming.Labels["lagoon.sh/environment"]
-				}
-				if label == "lagoon.sh/service" {
-					resource.Service = incoming.Labels["lagoon.sh/service"]
-				}
-
-				if label == "lagoon.sh/insightsType" {
-					insights.InputType = incoming.Labels["lagoon.sh/insightsType"]
-				}
-				if incoming.Labels["lagoon.sh/insightsType"] == "image-gz" {
-					insights.LagoonType = ImageFacts
-				}
-				if label == "lagoon.sh/insightsOutputCompressed" {
-					compressed, _ := strconv.ParseBool(incoming.Labels["lagoon.sh/insightsOutputCompressed"])
-					insights.OutputCompressed = compressed
-				}
-				if label == "lagoon.sh/insightsOutputFileMIMEType" {
-					insights.OutputFileMIMEType = incoming.Labels["lagoon.sh/insightsOutputFileMIMEType"]
-				}
-				if label == "lagoon.sh/insightsOutputFileExt" {
-					insights.OutputFileExt = incoming.Labels["lagoon.sh/insightsOutputFileExt"]
-				}
+			if label == "lagoon.sh/insightsType" {
+				insights.InputType = incoming.Labels["lagoon.sh/insightsType"]
+			}
+			if incoming.Labels["lagoon.sh/insightsType"] == "image-gz" {
+				insights.LagoonType = ImageFacts
+			}
+			if label == "lagoon.sh/insightsOutputCompressed" {
+				compressed, _ := strconv.ParseBool(incoming.Labels["lagoon.sh/insightsOutputCompressed"])
+				insights.OutputCompressed = compressed
+			}
+			if label == "lagoon.sh/insightsOutputFileMIMEType" {
+				insights.OutputFileMIMEType = incoming.Labels["lagoon.sh/insightsOutputFileMIMEType"]
+			}
+			if label == "lagoon.sh/insightsOutputFileExt" {
+				insights.OutputFileExt = incoming.Labels["lagoon.sh/insightsOutputFileExt"]
 			}
 		}
+	}
 
-		// Define insights type from incoming 'insightsType' label
-		if insights.InputType != "" {
-			switch insights.InputType {
-			case "sbom", "sbom-gz":
-				insights.InsightsType = Sbom
-			case "image", "image-gz":
-				insights.InsightsType = Image
-			case "direct":
-				insights.InsightsType = Direct
-			default:
-				insights.InsightsType = Raw
-			}
+	// Define insights type from incoming 'insightsType' label
+	if insights.InputType != "" {
+		switch insights.InputType {
+		case "sbom", "sbom-gz":
+			insights.InsightsType = Sbom
+		case "image", "image-gz":
+			insights.InsightsType = Image
+		case "direct":
+			insights.InsightsType = Direct
+		default:
+			insights.InsightsType = Raw
 		}
+	}
 
-		// Determine incoming payload type
-		if incoming.Payload == nil && incoming.BinaryPayload == nil {
-			if h.EnableDebug {
-				log.Printf("[DEBUG] no payload was found")
-			}
-			err := message.Reject(false)
-			if err != nil {
-				fmt.Printf("Unable to reject payload: %s\n", err.Error())
-			}
-			return
-		}
-		if len(incoming.Payload) != 0 {
-			insights.InputPayload = Payload
-		}
-		if len(incoming.BinaryPayload) != 0 {
-			insights.InputPayload = BinaryPayload
-		}
-
-		// Debug
+	// Determine incoming payload type
+	if incoming.Payload == nil && incoming.BinaryPayload == nil {
 		if h.EnableDebug {
-			log.Println("[DEBUG] insights:", insights)
-			log.Println("[DEBUG] target:", resource)
+			log.Printf("[DEBUG] no payload was found")
 		}
+		err := message.Reject(false)
+		if err != nil {
+			fmt.Printf("Unable to reject payload: %s\n", err.Error())
+		}
+		return
+	}
+	if len(incoming.Payload) != 0 {
+		insights.InputPayload = Payload
+	}
+	if len(incoming.BinaryPayload) != 0 {
+		insights.InputPayload = BinaryPayload
+	}
 
-		// Process s3 upload
-		if !h.S3Config.Disabled {
-			if insights.InsightsType != Direct {
-				err := h.sendToLagoonS3(incoming, insights, resource)
-				if err != nil {
-					log.Printf("Unable to send to S3: %s", err.Error())
-				}
+	// Debug
+	if h.EnableDebug {
+		log.Println("[DEBUG] insights:", insights)
+		log.Println("[DEBUG] target:", resource)
+	}
+
+	// Process s3 upload
+	if !h.S3Config.Disabled {
+		if insights.InsightsType != Direct {
+			err := h.sendToLagoonS3(incoming, insights, resource)
+			if err != nil {
+				log.Printf("Unable to send to S3: %s", err.Error())
 			}
 		}
+	}
 
-		// Process Lagoon API integration
-		if !h.LagoonAPI.Disabled {
-			if insights.InsightsType != Sbom &&
-				insights.InsightsType != Image &&
-				insights.InsightsType != Raw &&
-				insights.InsightsType != Direct {
-				log.Println("only 'sbom', 'direct', 'raw', and 'image' types are currently supported for api processing")
-			} else {
-				err := h.sendToLagoonAPI(incoming, resource, insights)
-				if err != nil {
-					log.Printf("Unable to send to the api: %s", err.Error())
-				}
+	// Process Lagoon API integration
+	if !h.LagoonAPI.Disabled {
+		if insights.InsightsType != Sbom &&
+			insights.InsightsType != Image &&
+			insights.InsightsType != Raw &&
+			insights.InsightsType != Direct {
+			log.Println("only 'sbom', 'direct', 'raw', and 'image' types are currently supported for api processing")
+		} else {
+			err := h.sendToLagoonAPI(incoming, resource, insights)
+			if err != nil {
+				log.Printf("Unable to send to the api: %s", err.Error())
 			}
 		}
 	}
