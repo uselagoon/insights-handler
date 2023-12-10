98e65281a6703eaaeaa97897bb64da5068c265a3
Blaize M Kaye <blaize.kaye@gmail.com>
2023-12-11 10:02:16 +1300

    Adds more services

--- internal/service/service.go
+++ internal/service/service.go
@@ -19,9 +19,8 @@ func SetupRouter(db *gorm.DB) (*gin.Engine, error) {
 	})
 
 	router.GET("/environment/:id/facts", GetFactsByEnvironmentEndpoint)
-	router.GET("/ping/{id}/facts", func(context *gin.Context) {
-		context.String(200, "pong")
-	})
+	router.POST("/environment/:id/facts", PostFactsByEnvironmentEndpoint)
+	router.DELETE("/environment/:id/facts", DeleteFactsByEnvironmentEndpoint)
 	return router, nil
 }
 
@@ -50,17 +49,108 @@ func setUpDatabase(opts dboptions) (*gorm.DB, error) {
 	return db, nil
 }
 
-//func CreateProblem(c *gin.Context) {
-//
-//}
-//
-//func GetProblem(c *gin.Context) {
-//
-//}
-//
-//func GetProblemsForEnvironment(c *gin.Context) {
-//
-//}
+// Fact represents the data model for a fact (assuming it's defined globally)
+
+func DeleteFactsByEnvironmentEndpoint(c *gin.Context) {
+	// Retrieve the environmentID parameter from the URL
+	environmentIDStr := c.Param("id")
+	environmentID, err := strconv.Atoi(environmentIDStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid environmentID"})
+		return
+	}
+
+	// Retrieve optional parameters from query string
+	//service := c.Query("service")
+	source := c.Query("source")
+
+	// Retrieve the GORM database instance from the Gin context
+	db, exists := c.Get("db")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection not found"})
+		return
+	}
+
+	// Cast the db interface to *gorm.DB
+	gormDB, ok := db.(*gorm.DB)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid database connection"})
+		return
+	}
+
+	// Build conditions for deletion based on optional parameters
+	conditions := map[string]interface{}{
+		"environment": environmentID,
+	}
+
+	//if service != "" {
+	//	conditions["lagoon_service"] = service
+	//}
+
+	if source != "" {
+		conditions["source"] = source
+	}
+
+	// Delete facts based on conditions
+	result := gormDB.Where(conditions).Delete(&lagoonclient.Fact{})
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete facts from the database"})
+		return
+	}
+
+	// Check if any records were deleted
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"message": "No matching facts found for deletion"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Facts deleted successfully"})
+}
+
+func PostFactsByEnvironmentEndpoint(c *gin.Context) {
+	// Retrieve the environmentID parameter from the URL
+	environmentIDStr := c.Param("id")
+	environmentID, err := strconv.Atoi(environmentIDStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid environmentID"})
+		return
+	}
+
+	// Retrieve the GORM database instance from the Gin context
+	db, exists := c.Get("db")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection not found"})
+		return
+	}
+
+	// Cast the db interface to *gorm.DB
+	gormDB, ok := db.(*gorm.DB)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid database connection"})
+		return
+	}
+
+	// Bind the JSON request body to a slice of Fact structs
+	var newFacts []lagoonclient.Fact
+	if err := c.ShouldBindJSON(&newFacts); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON format"})
+		return
+	}
+
+	// Set the environmentID for each new fact
+	for i := range newFacts {
+		newFacts[i].Environment = environmentID
+	}
+
+	// Create the new facts in the database
+	if err := gormDB.Create(&newFacts).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create facts in the database"})
+		return
+	}
+
+	// Return the newly created facts as JSON
+	c.JSON(http.StatusCreated, newFacts)
+}
 
 func GetFactsByEnvironmentEndpoint(c *gin.Context) {
 	// Retrieve the environmentID parameter from the URL
